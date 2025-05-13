29890539c005bc0fce4bb287075d0682cab4fac2
jeffreebs <berrocal.97@hotmail.com>
2025-05-13 17:06:56 -0600

    add instance and attributes

--- body.py
+++ body.py
@@ -1,6 +1,10 @@
 class Head:
     def __init__(self):
-        pass
+        self.eye= 2
+        self.nose= 1
+        self.mouth = 1
+        self.ear = 2
+        
 
 
 
@@ -11,7 +15,8 @@ class Torso:
 
 
 class Arm:
-    def __init__(self):
+    def __init__(self,side):
+        self.side = side
         pass
     
 
@@ -22,7 +27,8 @@ class Hand:
 
 
 class Leg:
-    def __init__(self):
+    def __init__(self,side):
+        self.side = side
         pass
     
 
@@ -34,17 +40,35 @@ class Feet:
 
 
 class Human:
-    def __init__(self):
+    def __init__(self,name):
+        self.name = name
         self.Head = Head()
         self.Torso = Torso()
-        self.Arm = Arm()
+        self.right_arm = Arm("right")
+        self.left_arm = Arm("left")
         self.Hand = Hand()
-        self.Leg = Leg()
+        self.right_leg = Leg("right")
+        self.left_leg = Leg("left")
         self.Feet = Feet()
-        pass
 
 
-person = Human()
-print (person.Head)
-print (person.Arm)
-print (person.Leg)
+    def describe(self):
+        print(f"Hello my name is {person.name} and i have  1 {self.left_arm.side} arm,  and 1 one more in {self.right_arm.side} side")
+        print (f"To, i have one {self.left_leg.side} leg  and another one in {self.right_leg.side} side ")
+
+
+    
+
+    
+
+
+person = Human("Jeff")
+
+person.describe()
+
+#This is another way to print but is a "poor practice"
+
+# print (f"Hello my name is{person.name} has one",  person.right_Arm.side,"arm")
+# print (f"Hello my {person.name} has one",person.left_Arm.side,"arm")
+# print (f"{person.name} has one",person.right_Leg.side,"leg")
+# print (f"{person.name} has one",person.left_Leg.side,"leg")
